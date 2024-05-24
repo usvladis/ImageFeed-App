ed07e19dc4454779d3cda9c076eafcc3d01c99e9
usvladis <usvladis@gmail.com>
2024-05-24 15:03:14 +0300

    Initial Commit

---
+++ WeddingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeddingApp
+//
+//  Created by Владислав Усачев on 24.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
